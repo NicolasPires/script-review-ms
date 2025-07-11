06c614462ff37a2fb53cdc8e350d05e85023b986
Nicolas Pires <nicolask.pires@gmail.com>
2025-07-10 23:19:44 -0300

    chore: initial project structure and environment setup

---
+++ src/main/java/com/cooperfilme/scriptreview/ScriptreviewApplication.java
@@ -0,0 +1,13 @@
+package com.cooperfilme.scriptreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooperfilme/scriptreview/ScriptreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperfilme.scriptreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
